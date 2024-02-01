b2b152c79fd78f25e02fcbe77943b1ac172d3ed1
Glitch (glitch-hello-website) <none>
2024-02-01 10:53:06 +0000

    👟🚔 Checkpoint ./main-background.js:54731710/3646 ./style.css:54731710/3209 ./index.html:54731710/2420

--- main-background.js
+++ main-background.js
@@ -3,10 +3,14 @@ let showText = 0;
 let r1 = 180;
 let g1 = 96;
 let b1 = 5;
+let r2 = 237;
+let g2 = 231;
+let b2 = 230;
 
 function toggleText() {
   var textContainer = document.getElementById("textContainer");
   textContainer.classList.toggle("hidden");
+  textContainer.classList.toggle("visible");
 }
 
 function setup() {
@@ -19,22 +23,22 @@ function draw() {
   textSize(36);
   noCursor();
   noStroke();
-  
+
   // Draw first circle
-  if (showText = 0) {
+  if (showText == 0) {
     fill(180, 96, 5);
     circle(windowWidth / 2, windowHeight / 2, windowWidth / 12);
   }
-  
+
   // Draw second circle
-  if (showText = 0) {
-    fill(237, 231, 230);
+  if (showText == 0) {
+    fill(r2, g2, b2);
     circle(windowWidth / 2, windowHeight / 2, windowWidth / 12.8);
   }
-  
+
   // Draw third circle following the mouse
   fill(r1, g1, b1);
-  circle(mouseX, mouseY, windowWidth / 64);
+  circle(mouseX, mouseY, windowWidth / 96);
 
   // Show text if the second circle is pressed
   // if (showText) {
@@ -48,10 +52,16 @@ function draw() {
     r1 = 17;
     g1 = 22;
     b1 = 44;
+    r2 = 180;
+    g2 = 96;
+    b2 = 5;
   } else {
     r1 = 180;
     g1 = 96;
     b1 = 5;
+    r2 = 237;
+    g2 = 231;
+    b2 = 230;
   }
 }
 
@@ -59,7 +69,6 @@ function mousePressed() {
   // Check if the mouse is over the second circle
   let d = dist(mouseX, mouseY, windowWidth / 2, windowHeight / 2);
   if (d < windowWidth / 25.6) {
-    showText = !showText; // Toggle the boolean value
+    showText = 1; // Toggle the boolean value
   }
 }
-
