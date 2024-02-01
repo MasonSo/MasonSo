a2de20093bcd6b4b55f4931bbe59c36896319f36
Glitch (glitch-hello-website) <none>
2024-02-01 09:56:54 +0000

    🏞👱 Checkpoint ./main-background.js:54731710/1700

--- main-background.js
+++ main-background.js
@@ -4,33 +4,18 @@ function setup() {
 
 function draw() {
   background(237, 231, 230);
-  let secondCircleRadius = windowWidth / 16.8;
-  let thirdCircleRadius = windowWidth / 64;
-  let boundaryDistance = 2.5;
-  let yOffset = windowHeight / 16; // Amount to move down
-  
+
+  noCursor();
+  // Draw first circle
+  noStroke();
   fill(180, 96, 5);
-  circle(windowWidth / 2, windowHeight / 16 + yOffset, secondCircleRadius + windowWidth / 24);
+  circle(windowWidth / 2, windowHeight / 2, windowWidth / 12);
   
+  // Draw second circle
   fill(237, 231, 230);
-  circle(windowWidth / 2, windowHeight / 16 + yOffset, secondCircleRadius - windowWidth / 6.4);
+  circle(windowWidth / 2, windowHeight / 2, windowWidth / 12.8);
   
-  let distance = dist(windowWidth / 2, windowHeight / 16 + yOffset, mouseX, mouseY);
-  let maxAllowedDistance = secondCircleRadius - thirdCircleRadius - boundaryDistance;
-  
-  if (distance <= maxAllowedDistance) {
-    noStroke();
-    fill(180, 96, 5);
-    // Centering the circle
-    circle(mouseX, mouseY, thirdCircleRadius);
-  } else {
-    let angle = atan2(mouseY - (windowHeight / 16 + yOffset), mouseX - windowWidth / 2);
-    let maxX = windowWidth / 2 + maxAllowedDistance * cos(angle);
-    let maxY = (windowHeight / 16 + yOffset) + maxAllowedDistance * sin(angle);
-    
-    noStroke();
-    fill(180, 96, 5);
-    // Centering the circle
-    circle(maxX, maxY, thirdCircleRadius);
-  }
+  // Draw third circle following the mouse
+  fill(180, 96, 5);
+  circle(mouseX, mouseY,windowWidth / 64);
 }
