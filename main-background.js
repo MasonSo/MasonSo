d2d575cd12b623cc1d18247ac8eecd1acd72e28b
Glitch (glitch-hello-website) <none>
2024-02-01 09:22:36 +0000

    👱🎒 Checkpoint ./main-cursor.js copy:54731710/2332 ./option-cursor.js:54731710/2332 ./main-background.js:54731710/10036 ./index.html:54731710/349

---
+++ main-background.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(237, 231, 230);
+  let secondCircleRadius = windowWidth / 16.8;
+  let thirdCircleRadius = windowWidth / 64;
+  let boundaryDistance = 2.5;
+  let yOffset = windowHeight / 16; // Amount to move down
+  
+  fill(180, 96, 5);
+  circle(windowWidth / 2, windowHeight / 16 + yOffset, secondCircleRadius + windowWidth / 24);
+  
+  fill(237, 231, 230);
+  circle(windowWidth / 2, windowHeight / 16 + yOffset, secondCircleRadius - windowWidth / 6.4);
+  
+  let distance = dist(windowWidth / 2, windowHeight / 16 + yOffset, mouseX, mouseY);
+  let maxAllowedDistance = secondCircleRadius - thirdCircleRadius - boundaryDistance;
+  
+  if (distance <= maxAllowedDistance) {
+    noStroke();
+    fill(180, 96, 5);
+    circle(mouseX, mouseY, thirdCircleRadius);
+  } else {
+    let angle = atan2(mouseY - (windowHeight / 16 + yOffset), mouseX - windowWidth / 2);
+    let maxX = windowWidth / 2 + maxAllowedDistance * cos(angle);
+    let maxY = (windowHeight / 16 + yOffset) + maxAllowedDistance * sin(angle);
+    
+    noStroke();
+    fill(180, 96, 5);
+    circle(maxX, maxY, thirdCircleRadius);
+  }
+}
