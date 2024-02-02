dad55a993541be6d59b97afd8c124de5b223ace9
Glitch (glitch-hello-website) <none>
2024-02-02 22:21:34 +0000

    🗾🐉 Checkpoint ./index.html:54731710/44 ./main-background.js:54731710/334 ./style.css:54731710/4921

--- main-background.js
+++ main-background.js
@@ -10,6 +10,8 @@ let b2 = 230;
 function toggleText() {
   var textContainer = document.getElementById("textContainer");
   textContainer.classList.toggle("visible");
+   var textContainer2 = document.getElementById("textContainer2");
+  textContainer2.classList.toggle("visible2");
 }
 
 function setup() {
